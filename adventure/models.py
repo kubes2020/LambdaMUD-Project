38c95b208051d5fe26c85a1bfd7a5113868962b1
Brian Kubes <briankubes@gmail.com>
2021-04-01 11:16:43 -0700

    preparing for heroku deploy

--- adventure/models.py
+++ adventure/models.py
@@ -8,6 +8,7 @@ import uuid
 class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
+    # img_url = models.CharField(max_length=500, default="path to image address")
     n_to = models.IntegerField(default=0)
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
