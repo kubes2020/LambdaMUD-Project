aa15f470153111bda4626b03c4b3da7c606ac250
Brian Kubes <briankubes@gmail.com>
2021-04-02 09:36:16 -0700

    broke the login again hopefully because @csrf was commented out, put it back in models.py

--- adventure/models.py
+++ adventure/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -54,14 +54,14 @@ class Player(models.Model):
             self.initialize()
             return self.room()
 
-# @csrf_exempt
+@csrf_exempt
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
     if created:
         Player.objects.create(user=instance)
         Token.objects.create(user=instance)
 
-# @csrf_exempt
+@csrf_exempt
 @receiver(post_save, sender=User)
 def save_user_player(sender, instance, **kwargs):
     instance.player.save()
